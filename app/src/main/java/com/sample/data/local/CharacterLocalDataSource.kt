7e43eb198939aa613d26569ff3c73a5df652cc03
Adam Danielczyk <12165583+adamdanielczyk@users.noreply.github.com>
2020-04-26 21:55:36 +0200

    Add local data source and database model

---
+++ app/src/main/java/com/sample/data/local/CharacterLocalDataSource.kt
@@ -0,0 +1,17 @@
+package com.sample.data.local
+
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class CharacterLocalDataSource @Inject constructor(private val characterDao: CharacterDao) {
+
+    fun getCharacters(): Flow<List<CharacterEntity>> = characterDao.getAll()
+
+    fun getCharacterById(id: Int): Flow<CharacterEntity> = characterDao.getById(id)
+
+    suspend fun insertCharacters(characters: List<CharacterEntity>) {
+        characterDao.insert(characters)
+    }
+}
