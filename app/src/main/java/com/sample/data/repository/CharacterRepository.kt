7ac5e149f6dd74340acf59c1a11310cd1732091a
Adam Danielczyk <12165583+adamdanielczyk@users.noreply.github.com>
2020-04-26 21:58:18 +0200

    Add CharacterRepository

---
+++ app/src/main/java/com/sample/data/repository/CharacterRepository.kt
@@ -0,0 +1,26 @@
+package com.sample.data.repository
+
+import com.sample.data.local.CharacterEntity
+import com.sample.data.local.CharacterLocalDataSource
+import com.sample.data.remote.CharacterRemoteDataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class CharacterRepository @Inject constructor(
+    private val localDataSource: CharacterLocalDataSource,
+    private val remoteDataSource: CharacterRemoteDataSource
+) {
+
+    fun getAllCharacters(): Flow<List<CharacterEntity>> {
+        return localDataSource.getCharacters().onEach { localCharacters ->
+            if (localCharacters.isEmpty()) {
+                val remoteCharacters = remoteDataSource.getAllCharacters().results
+                val newLocalCharacters = remoteCharacters.map(::CharacterEntity)
+                localDataSource.insertCharacters(newLocalCharacters)
+            }
+        }
+    }
+}
