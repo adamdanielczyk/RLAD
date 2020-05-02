d67d5c1fc20ead66e471f71fde9a5bdb531f6aa6
Adam Danielczyk <12165583+adamdanielczyk@users.noreply.github.com>
2020-05-03 00:49:19 +0200

    Add random night mode

--- app/src/main/java/com/sample/SampleApplication.kt
+++ app/src/main/java/com/sample/SampleApplication.kt
@@ -1,9 +1,11 @@
 package com.sample
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.sample.core.data.di.CoreComponent
 import com.sample.core.data.di.CoreComponentProvider
 import com.sample.core.data.di.DaggerCoreComponent
+import java.util.Random
 
 @Suppress("unused")
 class SampleApplication : Application(), CoreComponentProvider {
@@ -13,12 +15,21 @@ class SampleApplication : Application(), CoreComponentProvider {
     override fun onCreate() {
         super.onCreate()
         setupDagger()
+        setupRandomNightMode()
     }
 
     private fun setupDagger() {
         coreComponent = DaggerCoreComponent.builder()
             .appContext(this)
             .build()
+    }
+
+    private fun setupRandomNightMode() {
+        val nightMode = when (Random().nextBoolean()) {
+            true -> AppCompatDelegate.MODE_NIGHT_YES
+            false -> AppCompatDelegate.MODE_NIGHT_NO
+        }
+        AppCompatDelegate.setDefaultNightMode(nightMode)
     }
 
     override fun provideCoreComponent(): CoreComponent = coreComponent
