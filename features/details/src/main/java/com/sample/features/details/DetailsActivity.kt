e5dd551d10a36ad03bb2a2dbe4f40797a274080e
Adam Danielczyk <12165583+adamdanielczyk@users.noreply.github.com>
2022-04-23 20:59:15 +0200

    Migrate details screen to Compose

--- features/details/src/main/java/com/sample/features/details/DetailsActivity.kt
+++ features/details/src/main/java/com/sample/features/details/DetailsActivity.kt
@@ -1,21 +1,47 @@
 package com.sample.features.details
 
 import android.os.Bundle
-import android.view.MenuItem
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
-import androidx.lifecycle.lifecycleScope
-import com.bumptech.glide.Glide
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.sample.core.actions.EXTRA_CHARACTER_ID
-import com.sample.core.data.local.CharacterEntity
 import com.sample.core.data.local.CharacterEntity.Gender
 import com.sample.core.data.local.CharacterEntity.Status
-import com.sample.core.utils.applyDefaults
-import com.sample.features.details.databinding.DetailsActivityBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -30,61 +56,108 @@ class DetailsActivity : AppCompatActivity() {
         )
     }
 
-    private lateinit var binding: DetailsActivityBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DetailsActivityBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        viewModel.character
-            .onEach { displayCharacter(it) }
-            .launchIn(lifecycleScope)
-    }
-
-    private fun displayCharacter(entity: CharacterEntity) = with(entity) {
-        binding.collapsingToolbar.title = name
-
-        Glide.with(this@DetailsActivity)
-            .load(imageUrl)
-            .applyDefaults()
-            .into(binding.image)
-
-        binding.status.text = getString(
-            R.string.details_status,
-            when (status) {
-                Status.ALIVE -> getString(R.string.status_alive)
-                Status.DEAD -> getString(R.string.status_dead)
-                Status.UNKNOWN -> getString(R.string.unknown)
+        setContent {
+            MdcTheme {
+                DetailScreen()
             }
-        )
-        binding.species.text = getString(R.string.details_species, species)
-        binding.gender.text = getString(
-            R.string.details_gender,
-            when (gender) {
-                Gender.FEMALE -> getString(R.string.gender_female)
-                Gender.MALE -> getString(R.string.gender_male)
-                Gender.GENDERLESS -> getString(R.string.gender_genderless)
-                Gender.UNKNOWN -> getString(R.string.unknown)
-            }
-        )
-        binding.location.text = getString(R.string.details_location, location.name)
-        binding.created.text = getString(R.string.details_created, created)
-
-        with(binding.type) {
-            text = getString(R.string.details_type, type)
-            isVisible = !type.isNullOrBlank()
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) {
-            onBackPressed()
-            return true
+    @Composable
+    fun DetailScreen() {
+        val character = viewModel.character.collectAsState(initial = null).value ?: return
+
+        Scaffold(
+            topBar = {
+                val onBackPressedDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
+                TopAppBar(
+                    title = {
+                        Text(character.name)
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { onBackPressedDispatcher?.onBackPressed() }) {
+                            Icon(
+                                imageVector = Icons.Filled.ArrowBack,
+                                contentDescription = ""
+                            )
+                        }
+                    },
+                )
+            },
+        ) {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                ImageWithGradient(character.imageUrl)
+
+                Column(modifier = Modifier.padding(16.dp)) {
+                    DetailsText(stringResource(
+                        R.string.details_status,
+                        when (character.status) {
+                            Status.ALIVE -> getString(R.string.status_alive)
+                            Status.DEAD -> getString(R.string.status_dead)
+                            Status.UNKNOWN -> getString(R.string.unknown)
+                        })
+                    )
+
+                    DetailsText(stringResource(R.string.details_species, character.species))
+
+                    DetailsText(stringResource(
+                        R.string.details_gender,
+                        when (character.gender) {
+                            Gender.FEMALE -> getString(R.string.gender_female)
+                            Gender.MALE -> getString(R.string.gender_male)
+                            Gender.GENDERLESS -> getString(R.string.gender_genderless)
+                            Gender.UNKNOWN -> getString(R.string.unknown)
+                        })
+                    )
+
+                    DetailsText(stringResource(R.string.details_location, character.location.name))
+                    DetailsText(stringResource(R.string.details_created, character.created))
+
+                    val type = character.type
+                    if (!type.isNullOrBlank()) {
+                        DetailsText(stringResource(R.string.details_type, type))
+                    }
+                }
+            }
         }
-        return super.onOptionsItemSelected(item)
+    }
+
+    @Composable
+    fun ImageWithGradient(imageUrl: String) {
+        var sizeImage by remember { mutableStateOf(IntSize.Zero) }
+
+        val gradient = Brush.verticalGradient(
+            colors = listOf(Color.Transparent, Color(0x80000000)),
+            startY = sizeImage.height.toFloat() / 3,
+            endY = sizeImage.height.toFloat()
+        )
+
+        Box {
+            Image(
+                painter = rememberImagePainter(imageUrl),
+                contentDescription = "",
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .height(250.dp)
+                    .fillMaxWidth()
+                    .onGloballyPositioned {
+                        sizeImage = it.size
+                    },
+            )
+            Box(modifier = Modifier
+                .matchParentSize()
+                .background(gradient))
+        }
+    }
+
+    @Composable
+    fun DetailsText(text: String) {
+        Text(
+            text = text,
+            modifier = Modifier.padding(bottom = 8.dp),
+            style = MaterialTheme.typography.caption,
+        )
     }
 }
