c6380f40dfd94720cbec67bac37eb4575e2049ed
Adam Danielczyk <12165583+adamdanielczyk@users.noreply.github.com>
2022-04-24 01:43:03 +0200

    Remove compose theme adapter

--- app/src/main/java/com/sample/SampleApplication.kt
+++ app/src/main/java/com/sample/SampleApplication.kt
@@ -1,24 +1,7 @@
 package com.sample
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
-import java.util.Random
 
 @HiltAndroidApp
-class SampleApplication : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        setupRandomNightMode()
-    }
-
-    private fun setupRandomNightMode() {
-        val nightMode = if (Random().nextBoolean()) {
-            AppCompatDelegate.MODE_NIGHT_YES
-        } else {
-            AppCompatDelegate.MODE_NIGHT_NO
-        }
-        AppCompatDelegate.setDefaultNightMode(nightMode)
-    }
-}
+class SampleApplication : Application()
