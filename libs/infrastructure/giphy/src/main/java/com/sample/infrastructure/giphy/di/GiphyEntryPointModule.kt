73c8f60ddf1f3db2b170ec75bdeb0a18c3918449
Adam Danielczyk <12165583+adamdanielczyk@users.noreply.github.com>
2022-05-03 21:43:42 +0200

    Allow for multiple data sources

---
+++ libs/infrastructure/giphy/src/main/java/com/sample/infrastructure/giphy/di/GiphyEntryPointModule.kt
@@ -0,0 +1,27 @@
+package com.sample.infrastructure.giphy.di
+
+import com.sample.domain.model.DataSource
+import com.sample.domain.model.DataSourceKey
+import com.sample.domain.repository.ItemsRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.EntryPoints
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dagger.multibindings.IntoMap
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+internal object GiphyEntryPointModule {
+
+    @Singleton
+    @Provides
+    fun provideEntryPoint(componentBuilder: GiphyComponent.Builder): GiphyEntryPoint =
+        EntryPoints.get(componentBuilder.build(), GiphyEntryPoint::class.java)
+
+    @Provides
+    @IntoMap
+    @DataSourceKey(DataSource.GIPHY)
+    fun bindItemsRepository(entryPoint: GiphyEntryPoint): ItemsRepository = entryPoint.provideGiphyRepository()
+}
--- libs/infrastructure/rickandmorty/src/main/java/com/sample/infrastructure/rickandmorty/di/RickAndMortyModule.kt
+++ libs/infrastructure/rickandmorty/src/main/java/com/sample/infrastructure/rickandmorty/di/RickAndMortyModule.kt
@@ -2,29 +2,24 @@ package com.sample.infrastructure.rickandmorty.di
 
 import android.content.Context
 import androidx.room.Room
-import com.sample.domain.repository.ItemsRepository
 import com.sample.infrastructure.rickandmorty.local.CharacterDao
 import com.sample.infrastructure.rickandmorty.local.CharacterDatabase
 import com.sample.infrastructure.rickandmorty.remote.RickAndMortyApi
-import com.sample.infrastructure.rickandmorty.repository.CharacterRepository
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import javax.inject.Singleton
 
-@InstallIn(SingletonComponent::class)
-@Module(includes = [DataModule.Bindings::class])
-internal object DataModule {
+@InstallIn(RickAndMortyComponent::class)
+@Module
+internal object RickAndMortyModule {
 
     @Provides
-    @Singleton
+    @RickAndMortyScope
     fun characterDatabase(@ApplicationContext context: Context): CharacterDatabase {
         return Room.databaseBuilder(
             context,
@@ -34,12 +29,12 @@ internal object DataModule {
     }
 
     @Provides
-    @Singleton
+    @RickAndMortyScope
     fun characterDao(characterDatabase: CharacterDatabase): CharacterDao =
         characterDatabase.characterDao()
 
     @Provides
-    @Singleton
+    @RickAndMortyScope
     fun moshi(): Moshi {
         return Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
@@ -47,7 +42,7 @@ internal object DataModule {
     }
 
     @Provides
-    @Singleton
+    @RickAndMortyScope
     fun retrofit(moshi: Moshi): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://rickandmortyapi.com")
@@ -56,15 +51,7 @@ internal object DataModule {
     }
 
     @Provides
-    @Singleton
+    @RickAndMortyScope
     fun rickAndMortyApi(retrofit: Retrofit): RickAndMortyApi =
         retrofit.create(RickAndMortyApi::class.java)
-
-    @InstallIn(SingletonComponent::class)
-    @Module
-    internal interface Bindings {
-
-        @Binds
-        fun bindItemsRepository(impl: CharacterRepository): ItemsRepository
-    }
 }
