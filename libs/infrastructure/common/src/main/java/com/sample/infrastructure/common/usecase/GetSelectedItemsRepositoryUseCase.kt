ceb7b38b411b6b7aee763544e2ff508bb4dd0308
Adam Danielczyk <12165583+adamdanielczyk@users.noreply.github.com>
2022-05-04 23:06:20 +0200

    Sort items repositories

--- libs/infrastructure/common/src/main/java/com/sample/infrastructure/common/usecase/GetSelectedItemsRepositoryUseCase.kt
+++ libs/infrastructure/common/src/main/java/com/sample/infrastructure/common/usecase/GetSelectedItemsRepositoryUseCase.kt
@@ -5,14 +5,15 @@ import com.sample.infrastructure.common.repository.ItemsRepository
 import javax.inject.Inject
 
 internal class GetSelectedItemsRepositoryUseCase @Inject constructor(
-    private val repositories: Set<@JvmSuppressWildcards ItemsRepository>,
+    private val getAllItemsRepositoriesUseCase: GetAllItemsRepositoriesUseCase,
     private val appSettingsRepository: AppSettingsRepository,
 ) {
 
     suspend operator fun invoke(): ItemsRepository {
         val selectedDataSourceName = appSettingsRepository.getSelectedDataSourceName()
-        return repositories.firstOrNull { itemsRepository ->
+        val allItemsRepositories = getAllItemsRepositoriesUseCase()
+        return allItemsRepositories.firstOrNull { itemsRepository ->
             itemsRepository.getDataSourceName() == selectedDataSourceName
-        } ?: repositories.minByOrNull(ItemsRepository::getDataSourceName)!!
+        } ?: allItemsRepositories.first()
     }
 }
