9cc73310176454e80d4941e0da33ae242992c5e3
Adam Danielczyk <12165583+adamdanielczyk@users.noreply.github.com>
2022-10-10 02:08:37 +0200

    Share dependencies between application and feature modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,38 +30,4 @@ dependencies {
 
     testImplementation(projects.libs.testutils)
     androidTestImplementation(projects.libs.testutils)
-
-    implementation(libs.kotlin.stdlib)
-
-    implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.kotlinx.coroutines.android)
-
-    implementation(libs.androidx.paging.runtime)
-    implementation(libs.androidx.paging.compose)
-
-    implementation(libs.androidx.hilt.navigation.compose)
-    implementation(libs.androidx.navigation.compose)
-
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.activity.compose)
-
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.tooling)
-    implementation(libs.androidx.compose.foundation)
-    implementation(libs.androidx.compose.material)
-    implementation(libs.androidx.compose.material.icons)
-
-    implementation(libs.androidx.lifecycle.runtime)
-    implementation(libs.androidx.lifecycle.viewmodel)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-
-    implementation(libs.accompanist.systemuicontroller)
-    implementation(libs.accompanist.swiperefresh)
-
-    implementation(libs.coil.compose)
-    implementation(libs.coil.gif)
-
-    implementation(libs.android.material)
-
-    implementation(libs.lottie)
 }
