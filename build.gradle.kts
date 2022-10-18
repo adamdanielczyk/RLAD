e263c14cd1f052367c1a4d8399c6a6131972b2fa
Adam Danielczyk <12165583+adamdanielczyk@users.noreply.github.com>
2022-10-19 00:18:44 +0200

    Remove dependencyUpdates task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,5 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.dependencyVersions)
-}
-
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
-}
-
-tasks.named("dependencyUpdates", DependencyUpdatesTask::class.java).configure {
-    checkForGradleUpdate = true
-    rejectVersionIf { isNonStable(candidate.version) }
 }
