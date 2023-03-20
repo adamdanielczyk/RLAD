d61f470d8394884a8e7af8ff337a12063e911b1e
Adam Danielczyk <12165583+adamdanielczyk@users.noreply.github.com>
2023-03-20 18:57:35 +0100

    Remove convention module from build-logic

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -2,11 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
 dependencies {
     implementation(libs.android.gradlePlugin)
     implementation(libs.hilt.gradlePlugin)
