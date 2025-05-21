f25cb2e35e62c2fc5062ceb481e9af7aafb4fda4
Adam Danielczyk <12165583+adamdanielczyk@users.noreply.github.com>
2025-05-21 22:37:04 +0200

    define compose plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.room) apply false
 }
