6195175655cfd7eaf366676d62282b57e6662ca0
Adam Danielczyk <12165583+adamdanielczyk@users.noreply.github.com>
2025-12-05 18:55:36 +0100

    remove blank chars

--- native-android/core/infrastructure/common/src/test/java/com/rlad/core/infrastructure/common/usecase/GetSelectedDataSourceUseCaseImplTest.kt
+++ native-android/core/infrastructure/common/src/test/java/com/rlad/core/infrastructure/common/usecase/GetSelectedDataSourceUseCaseImplTest.kt
@@ -7,7 +7,7 @@ import io.mockk.mockk
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals        
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class GetSelectedDataSourceUseCaseImplTest {
