f556284b094faeeebe7e9fc7c523a1689b8f4a9b
Adam Danielczyk <12165583+adamdanielczyk@users.noreply.github.com>
2025-12-09 19:36:51 +0100

    Remove redundant Build.VERSION.SDK_INT check

--- native-android/app/src/main/java/com/rlad/RladApplication.kt
+++ native-android/app/src/main/java/com/rlad/RladApplication.kt
@@ -2,11 +2,9 @@ package com.rlad
 
 import android.app.Application
 import android.graphics.Color
-import android.os.Build
 import androidx.core.graphics.drawable.toDrawable
 import coil.ImageLoader
 import coil.ImageLoaderFactory
-import coil.decode.GifDecoder
 import coil.decode.ImageDecoderDecoder
 import com.rlad.di.AppGraph
 import dev.zacsweers.metro.createGraphFactory
@@ -26,13 +24,7 @@ internal class RladApplication : Application(), ImageLoaderFactory, MetroApplica
             .placeholder(Color.LTGRAY.toDrawable())
             .fallback(Color.GRAY.toDrawable())
             .error(Color.RED.toDrawable())
-            .components {
-                if (Build.VERSION.SDK_INT >= 28) {
-                    add(ImageDecoderDecoder.Factory())
-                } else {
-                    add(GifDecoder.Factory())
-                }
-            }
+            .components { add(ImageDecoderDecoder.Factory()) }
             .build()
     }
 }
